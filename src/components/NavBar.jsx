98e035a832e711eaa53b635df7c0527dc8560139
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-04-23 23:38:15 +0530

    added few features in the navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,12 @@ function NavBar() {
     <heder>
       <nav>
         <img src={appleImg} alt="Apple" width={14} height={18} />
+
+        <div>
+          {["iPhones", "Macbooks", "Tablets"].map((nav) => (
+            <div key={nav}>{nav}</div>
+          ))}
+        </div>
       </nav>
     </heder>
   );
