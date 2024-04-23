6d5657a3101e34fcf07dc401e8a62138afcafc26
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-04-23 23:34:07 +0530

    updated the NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import { appleImg } from "./../utils";
+
+function NavBar() {
+  return (
+    <heder>
+      <nav>
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+      </nav>
+    </heder>
+  );
+}
+
+export default NavBar;
