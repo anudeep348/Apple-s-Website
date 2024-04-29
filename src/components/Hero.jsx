878f1f42a6811d2faee3c0e03c0fdbfd0ba0f98d
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-04-29 22:02:54 +0530

    Added few blocks in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
 function Hero() {
   return (
-    <section className="w-full nav-height bg-black relative">Hero</section>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">iPhone 15 Pro</p>
+      </div>
+    </section>
   );
 }
 
