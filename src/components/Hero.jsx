06e58bdd5c0c1681c8cbbfba60f0cc59b5070d54
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-04-30 22:59:21 +0530

    Completed the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,13 @@ function Hero() {
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
-      delay: 1.5,
+      delay: 2,
+    });
+
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2,
     });
   }, []);
   return (
@@ -35,7 +41,7 @@ function Hero() {
         <p id="hero" className="hero-title">
           iPhone 15 Pro
         </p>
-        <div className="md:w-10/12 w-9/12">
+        <div className="md:w-10/12 w-8/12">
           <video
             autoPlay
             muted
@@ -46,6 +52,16 @@ function Hero() {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
